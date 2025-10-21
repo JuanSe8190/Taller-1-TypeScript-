5af57293a2c08722dbae85dce090531e9fcc8ebf
Juancho-bas <js.avendanor1@uniandes.edu.co>
2025-10-20 21:18:25 -0500

    desde el princiipio

--- Taller/Serie.ts
+++ Taller/Serie.ts
@@ -8,4 +8,10 @@ export class Serie {
     public link: string,
     public imageUrl: string
   ) {}
+
+  static promedioSeasons(series: Serie[]): number {
+        if (series.length === 0) return 0;
+        const totalSeasons = series.reduce((sum, serie) => sum + serie.seasons, 0);
+        return totalSeasons / series.length;
+  }
 }
